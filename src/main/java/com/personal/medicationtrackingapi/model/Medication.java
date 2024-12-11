1710e0fcfe9057d3211343eac707d28b84732e06
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-12-11 17:49:28 +0000

    fix: added missing dependencies to pom.xml

--- src/main/java/com/personal/medicationtrackingapi/model/Medication.java
+++ src/main/java/com/personal/medicationtrackingapi/model/Medication.java
@@ -1,4 +1,24 @@
 package com.personal.medicationtrackingapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Medication {
+
+    @Id
+    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_generator")
+    @SequenceGenerator(name = "id_generator", allocationSize = 1)
+    private Long id;
+
+    @Column
+    private String medicationName;
+
+    @Column
+    private int medicationDosage;
 }
