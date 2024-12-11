d994bf67f7662e2b280253e50e393f944bc592cf
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-12-11 17:52:36 +0000

    feat: define the repository in MedicationManagerRepository class

--- src/main/java/com/personal/medicationtrackingapi/model/Medication.java
+++ src/main/java/com/personal/medicationtrackingapi/model/Medication.java
@@ -21,5 +21,4 @@ public class Medication {
 
     @Column
     private int medicationDosage;
-
 }
--- src/main/java/com/personal/medicationtrackingapi/repository/MedicationManagerRepository.java
+++ src/main/java/com/personal/medicationtrackingapi/repository/MedicationManagerRepository.java
@@ -1,4 +1,9 @@
 package com.personal.medicationtrackingapi.repository;
 
-public interface MedicationManagerRepository {
+import com.personal.medicationtrackingapi.model.Medication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicationManagerRepository extends CrudRepository<Medication, Long> {
 }
