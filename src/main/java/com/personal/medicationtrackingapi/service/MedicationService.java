ea182665b020d5c0cb0287eddad5e0efc37f5dd1
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-12-11 18:27:07 +0000

    feat: add getAllMedications function in MedicationService

--- src/main/java/com/personal/medicationtrackingapi/service/MedicationService.java
+++ src/main/java/com/personal/medicationtrackingapi/service/MedicationService.java
@@ -1,4 +1,10 @@
 package com.personal.medicationtrackingapi.service;
 
+import com.personal.medicationtrackingapi.model.Medication;
+
+import java.util.List;
+
 public interface MedicationService {
+
+    List<Medication> getAllMedications();
 }
